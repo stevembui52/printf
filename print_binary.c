27927df99d2b2ca58ef9c7cfc5fa851a848a2288
stevembui52 <stevembui52@gmail.com>
2023-07-28 03:36:19 -0700

    print

--- print_binary.c
+++ print_binary.c
@@ -2,12 +2,11 @@
 /**
  * print_bnr - prints a binary number
  *@n:the number to be printed
- *Return: 0 on success
+ *Return:the binary number
  */
 int print_bnr(int n)
 {
 	if (n > 1)
 		print_bnr(n / 2);
-	print_chr('0' + n % 2);
-	return (0);
+	return(print_chr('0' + n % 2));
 }
